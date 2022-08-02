ac504b5c160e10bed586a139edf6fcb711076dbb
Eloy Coto <eloy.coto@acalustra.com>
2022-08-02 13:12:17 +0200

    Cleanup ignition
    Signed-off-by: Eloy Coto <eloy.coto@acalustra.com>

--- pkg/ignition/source/platform/platform.go
+++ pkg/ignition/source/platform/platform.go
@@ -20,27 +20,28 @@ import (
 
 	"github.com/eloycoto/ignition_poc/pkg/ignition/source/log"
 	"github.com/eloycoto/ignition_poc/pkg/ignition/source/providers"
-	"github.com/eloycoto/ignition_poc/pkg/ignition/source/providers/aliyun"
-	"github.com/eloycoto/ignition_poc/pkg/ignition/source/providers/aws"
-	"github.com/eloycoto/ignition_poc/pkg/ignition/source/providers/azure"
-	"github.com/eloycoto/ignition_poc/pkg/ignition/source/providers/azurestack"
-	"github.com/eloycoto/ignition_poc/pkg/ignition/source/providers/cloudstack"
-	"github.com/eloycoto/ignition_poc/pkg/ignition/source/providers/digitalocean"
-	"github.com/eloycoto/ignition_poc/pkg/ignition/source/providers/exoscale"
+
+	// "github.com/eloycoto/ignition_poc/pkg/ignition/source/providers/aliyun"
+	// "github.com/eloycoto/ignition_poc/pkg/ignition/source/providers/aws"
+	// "github.com/eloycoto/ignition_poc/pkg/ignition/source/providers/azure"
+	// "github.com/eloycoto/ignition_poc/pkg/ignition/source/providers/azurestack"
+	// "github.com/eloycoto/ignition_poc/pkg/ignition/source/providers/cloudstack"
+	// "github.com/eloycoto/ignition_poc/pkg/ignition/source/providers/digitalocean"
+	// "github.com/eloycoto/ignition_poc/pkg/ignition/source/providers/exoscale"
 	"github.com/eloycoto/ignition_poc/pkg/ignition/source/providers/file"
-	"github.com/eloycoto/ignition_poc/pkg/ignition/source/providers/gcp"
-	"github.com/eloycoto/ignition_poc/pkg/ignition/source/providers/ibmcloud"
-	"github.com/eloycoto/ignition_poc/pkg/ignition/source/providers/kubevirt"
-	"github.com/eloycoto/ignition_poc/pkg/ignition/source/providers/noop"
-	"github.com/eloycoto/ignition_poc/pkg/ignition/source/providers/nutanix"
-	"github.com/eloycoto/ignition_poc/pkg/ignition/source/providers/openstack"
-	"github.com/eloycoto/ignition_poc/pkg/ignition/source/providers/packet"
-	"github.com/eloycoto/ignition_poc/pkg/ignition/source/providers/powervs"
-	"github.com/eloycoto/ignition_poc/pkg/ignition/source/providers/qemu"
-	"github.com/eloycoto/ignition_poc/pkg/ignition/source/providers/virtualbox"
-	"github.com/eloycoto/ignition_poc/pkg/ignition/source/providers/vmware"
-	"github.com/eloycoto/ignition_poc/pkg/ignition/source/providers/vultr"
-	"github.com/eloycoto/ignition_poc/pkg/ignition/source/providers/zvm"
+	// "github.com/eloycoto/ignition_poc/pkg/ignition/source/providers/gcp"
+	// "github.com/eloycoto/ignition_poc/pkg/ignition/source/providers/ibmcloud"
+	// "github.com/eloycoto/ignition_poc/pkg/ignition/source/providers/kubevirt"
+	// "github.com/eloycoto/ignition_poc/pkg/ignition/source/providers/noop"
+	// "github.com/eloycoto/ignition_poc/pkg/ignition/source/providers/nutanix"
+	// "github.com/eloycoto/ignition_poc/pkg/ignition/source/providers/openstack"
+	// "github.com/eloycoto/ignition_poc/pkg/ignition/source/providers/packet"
+	// "github.com/eloycoto/ignition_poc/pkg/ignition/source/providers/powervs"
+	// "github.com/eloycoto/ignition_poc/pkg/ignition/source/providers/qemu"
+	// "github.com/eloycoto/ignition_poc/pkg/ignition/source/providers/virtualbox"
+	// "github.com/eloycoto/ignition_poc/pkg/ignition/source/providers/vmware"
+	// "github.com/eloycoto/ignition_poc/pkg/ignition/source/providers/vultr"
+	// "github.com/eloycoto/ignition_poc/pkg/ignition/source/providers/zvm"
 	"github.com/eloycoto/ignition_poc/pkg/ignition/source/registry"
 	"github.com/eloycoto/ignition_poc/pkg/ignition/source/resource"
 )
@@ -110,99 +111,99 @@ func (c Config) DelConfig(f *resource.Fetcher) error {
 var configs = registry.Create("platform configs")
 
 func init() {
-	configs.Register(Config{
-		name:  "aliyun",
-		fetch: aliyun.FetchConfig,
-	})
-	configs.Register(Config{
-		name:       "aws",
-		fetch:      aws.FetchConfig,
-		init:       aws.Init,
-		newFetcher: aws.NewFetcher,
-	})
-	configs.Register(Config{
-		name:  "azure",
-		fetch: azure.FetchConfig,
-	})
-	configs.Register(Config{
-		name:  "azurestack",
-		fetch: azurestack.FetchConfig,
-	})
-	configs.Register(Config{
-		name:  "brightbox",
-		fetch: openstack.FetchConfig,
-	})
-	configs.Register(Config{
-		name:  "cloudstack",
-		fetch: cloudstack.FetchConfig,
-	})
-	configs.Register(Config{
-		name:  "digitalocean",
-		fetch: digitalocean.FetchConfig,
-	})
-	configs.Register(Config{
-		name:  "exoscale",
-		fetch: exoscale.FetchConfig,
-	})
+	// configs.Register(Config{
+	// 	name:  "aliyun",
+	// 	fetch: aliyun.FetchConfig,
+	// })
+	// configs.Register(Config{
+	// 	name:       "aws",
+	// 	fetch:      aws.FetchConfig,
+	// 	init:       aws.Init,
+	// 	newFetcher: aws.NewFetcher,
+	// })
+	// configs.Register(Config{
+	// 	name:  "azure",
+	// 	fetch: azure.FetchConfig,
+	// })
+	// configs.Register(Config{
+	// 	name:  "azurestack",
+	// 	fetch: azurestack.FetchConfig,
+	// })
+	// configs.Register(Config{
+	// 	name:  "brightbox",
+	// 	fetch: openstack.FetchConfig,
+	// })
+	// configs.Register(Config{
+	// 	name:  "cloudstack",
+	// 	fetch: cloudstack.FetchConfig,
+	// })
+	// configs.Register(Config{
+	// 	name:  "digitalocean",
+	// 	fetch: digitalocean.FetchConfig,
+	// })
+	// configs.Register(Config{
+	// 	name:  "exoscale",
+	// 	fetch: exoscale.FetchConfig,
+	// })
 	configs.Register(Config{
 		name:  "file",
 		fetch: file.FetchConfig,
 	})
-	configs.Register(Config{
-		name:  "gcp",
-		fetch: gcp.FetchConfig,
-	})
-	configs.Register(Config{
-		name:  "ibmcloud",
-		fetch: ibmcloud.FetchConfig,
-	})
-	configs.Register(Config{
-		name:  "kubevirt",
-		fetch: kubevirt.FetchConfig,
-	})
-	configs.Register(Config{
-		name:  "metal",
-		fetch: noop.FetchConfig,
-	})
-	configs.Register(Config{
-		name:  "nutanix",
-		fetch: nutanix.FetchConfig,
-	})
-	configs.Register(Config{
-		name:  "openstack",
-		fetch: openstack.FetchConfig,
-	})
-	configs.Register(Config{
-		name:   "packet",
-		fetch:  packet.FetchConfig,
-		status: packet.PostStatus,
-	})
-	configs.Register(Config{
-		name:  "powervs",
-		fetch: powervs.FetchConfig,
-	})
-	configs.Register(Config{
-		name:  "qemu",
-		fetch: qemu.FetchConfig,
-	})
-	configs.Register(Config{
-		name:      "virtualbox",
-		fetch:     virtualbox.FetchConfig,
-		delConfig: virtualbox.DelConfig,
-	})
-	configs.Register(Config{
-		name:      "vmware",
-		fetch:     vmware.FetchConfig,
-		delConfig: vmware.DelConfig,
-	})
-	configs.Register(Config{
-		name:  "vultr",
-		fetch: vultr.FetchConfig,
-	})
-	configs.Register(Config{
-		name:  "zvm",
-		fetch: zvm.FetchConfig,
-	})
+	// configs.Register(Config{
+	// 	name:  "gcp",
+	// 	fetch: gcp.FetchConfig,
+	// })
+	// configs.Register(Config{
+	// 	name:  "ibmcloud",
+	// 	fetch: ibmcloud.FetchConfig,
+	// })
+	// configs.Register(Config{
+	// 	name:  "kubevirt",
+	// 	fetch: kubevirt.FetchConfig,
+	// })
+	// configs.Register(Config{
+	// 	name:  "metal",
+	// 	fetch: noop.FetchConfig,
+	// })
+	// configs.Register(Config{
+	// 	name:  "nutanix",
+	// 	fetch: nutanix.FetchConfig,
+	// })
+	// configs.Register(Config{
+	// 	name:  "openstack",
+	// 	fetch: openstack.FetchConfig,
+	// })
+	// configs.Register(Config{
+	// 	name:   "packet",
+	// 	fetch:  packet.FetchConfig,
+	// 	status: packet.PostStatus,
+	// })
+	// configs.Register(Config{
+	// 	name:  "powervs",
+	// 	fetch: powervs.FetchConfig,
+	// })
+	// configs.Register(Config{
+	// 	name:  "qemu",
+	// 	fetch: qemu.FetchConfig,
+	// })
+	// configs.Register(Config{
+	// 	name:      "virtualbox",
+	// 	fetch:     virtualbox.FetchConfig,
+	// 	delConfig: virtualbox.DelConfig,
+	// })
+	// configs.Register(Config{
+	// 	name:      "vmware",
+	// 	fetch:     vmware.FetchConfig,
+	// 	delConfig: vmware.DelConfig,
+	// })
+	// configs.Register(Config{
+	// 	name:  "vultr",
+	// 	fetch: vultr.FetchConfig,
+	// })
+	// configs.Register(Config{
+	// 	name:  "zvm",
+	// 	fetch: zvm.FetchConfig,
+	// })
 }
 
 func Get(name string) (config Config, ok bool) {
